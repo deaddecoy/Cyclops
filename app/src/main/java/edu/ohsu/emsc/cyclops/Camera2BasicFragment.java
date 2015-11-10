26f4ba39dc2621e860e871e1cea749aa2ebf0c53
deaddecoy <deaddecoy@gmail.com>
2015-11-09 18:26:42 -0800

    Creating new project Cyclops to play around with Android cameras.

---
+++ app/src/main/java/edu/ohsu/emsc/cyclops/Camera2BasicFragment.java
@@ -0,0 +1,104 @@
+package edu.ohsu.emsc.cyclops;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.graphics.ImageFormat;
+import android.graphics.Matrix;
+import android.graphics.RectF;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
+import android.support.v13.app.FragmentCompat;
+import android.util.Log;
+import android.util.Size;
+import android.util.SparseIntArray;
+import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by deaddecoy on 9/26/2015.
+ */
+public class Camera2BasicFragment extends Fragment
+    implements View.OnClickListener, FragmentCompat.OnRequestPermissionsResultCallback {
+
+    /**
+     * Conversion from screen rotation to JPEG orientation.
+     */
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+    private static final String FRAGMENT_DIALOG = "dialog";
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0,    90);
+        ORIENTATIONS.append(Surface.ROTATION_90,    0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+
+    /**
+     * Tag for the (@link Log).
+     */
+    private static final String TAG = "Camera2BasicFragment";
+    /**
+     * Camera state: Showing camera preview
+     */
+    private static final int STATE_PREVIEW = 0;
+    /**
+     * Camera state: Waiting for the focus to be locked.
+     */
+    private static final int STATE_WAITING_LOCK = 1;
+    /**
+     * Camera state: Waiting for the exposure to be in the precaputure state.
+     */
+    private static final int STATE_WAITING_PRECAPTURE = 3;
+    /**
+     * Camera state: Waiting for the exposure state to be something other than precapture.
+     */
+    private static final int STATE_WAITING_NON_PRECAPTURE = 3;
+    /**
+     * Camera state: Picture was taken.
+     */
+    private static final int STATE_PICTURE_TAKEN = 4;
+
+    public static Camera2BasicFragment newInstance() {
+        return new Camera2BasicFragment();
+    }
+}
---
+++ app/src/main/java/edu/ohsu/emsc/cyclops/CameraActivity.java
@@ -0,0 +1,29 @@
+package edu.ohsu.emsc.cyclops;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Steps for using the camera:
+ *
+ * Detect and Access Camera
+ * Create a Preview Class
+ * Build a Preview Layout
+ * Setup Listeners for Capture
+ * Capture and Save Files
+ * Release the Camera
+ *
+ */
+public class CameraActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
+    }
+}
